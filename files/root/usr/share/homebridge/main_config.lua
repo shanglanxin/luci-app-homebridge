394af57433c038c4bda995b6f4fc9b4085190b72
gys <shanglanxin@gmail.com>
2020-03-12 15:00:43 +0800

    init for 0.1 version

---
+++ files/root/usr/share/homebridge/main_config.lua
@@ -0,0 +1,75 @@
+local ucursor = require "luci.model.uci".cursor()
+local json = require "luci.jsonc"
+
+local h_interface = ucursor:get("homebridge", "@homebridge[0]", "interface")
+
+local platform_section=arg[1]
+
+local platform_info = ucursor:get_all("homebridge", platform_section)
+
+local device_type
+if platform_info.platform_type == "ReYeelightPlatform" then
+device_type = platform_info.platform_yeelight
+end
+if platform_info.platform_type == "MiPhilipsLightPlatform" then
+device_type = platform_info.platform_philips_light
+end
+if platform_info.platform_type == "MiOutletPlatform" then
+device_type = platform_info.platform_mi_plug
+end
+
+local platform = {
+		platform = platform_info.platform_type,
+		deviceCfgs = {{
+			type = device_type,
+			ip = platform_info.ip,
+			token = platform_info.token
+}}
+
+}
+
+if platform_info.platform_type == "ReYeelightPlatform" then
+platform.deviceCfgs[1]["Name"] = platform_info.alias
+platform.deviceCfgs[1]["updatetimer"] = (platform_info.updatetimer == "1")
+platform.deviceCfgs[1]["interval"] = tonumber(platform_info.interval)
+end
+
+if platform_info.platform_type == "MiPhilipsLightPlatform" then
+	
+	if platform_info.platform_philips_light == "MiPhilipsCeilingLamp" or platform_info.platform_philips_light == "MiPhilipsSmartBulb" then
+	platform.deviceCfgs[1]["lightName"] = platform_info.alias
+	platform.deviceCfgs[1]["lightDisable"] = false
+	end
+
+	if platform_info.platform_philips_light == "MiPhilipsCeilingLamp" then
+	platform.deviceCfgs[1]["updatetimer"] = (platform_info.updatetimer == "1") 
+	platform.deviceCfgs[1]["interval"] = tonumber(platform_info.interval) 
+	end 
+
+	if platform_info.platform_philips_light == "MiPhilipsTableLamp2" then
+	platform.deviceCfgs[1]["mainLightName"] = platform_info.main_light_name
+	platform.deviceCfgs[1]["secondLightName"] = platform_info.second_light_name
+	platform.deviceCfgs[1]["secondLightDisable"] = platform_info.second_light_disable == "1"
+	platform.deviceCfgs[1]["eyecareSwitchName"] = platform_info.eyecare_switch_name
+	platform.deviceCfgs[1]["eyecareSwitchDisable"] = platform_info.eyecare_switch_disable == "1"
+	end
+end
+
+if platform_info.platform_type == "MiOutletPlatform" then
+platform.deviceCfgs[1]["outletName"] = platform_info.alias
+platform.deviceCfgs[1]["outletDisable"] = platform_info.outlet_disable == "1"
+platform.deviceCfgs[1]["temperatureName"] = platform_info.temperature_name
+platform.deviceCfgs[1]["temperatureDisable"] = platform_info.temperature_disable
+
+	if platform_info.platform_mi_plug == "MiPlugBase" or platform_info.platform_mi_plug == "MiIntelligencePinboard" then
+		platform.deviceCfgs[1]["switchLEDName"] = platform_info.switch_LED_name
+		platform.deviceCfgs[1]["switchLEDDisable"] = platform_info.switch_LED_disable == "1"
+	end
+	if platform_info.platform_mi_plug == "MiPlugBaseWithUSB" then
+		platform.deviceCfgs[1]["switchUSBName"] = platform_info.switch_USB_name
+		platform.deviceCfgs[1]["switchUSBDisable"] = platform_info.switch_USB_disable == "1"
+	end
+
+end
+
+print(json.stringify(platform, 1))
